eb772dcfe7e40b6046f342c3b1e4533ca1b7c1c3
Will Papper <contact@papper.me>
2017-10-28 15:46:27 -0700

    Added vibration sequence

--- Haptic/App.js
+++ Haptic/App.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View, Vibration, TouchableHighlight, ScrollView, Alert } from 'react-native';
-import Tts from 'react-native-tts';
 
 const height = '100%';
 const width = '100%';
+
 export default class App extends React.Component {
 
   handleScroll(event) {
     // Assume that position always starts at the top
     // Starting point for Y is 0, ending point for Y is -200
     var yCoordinate = event.nativeEvent.contentOffset.y
-    var yCoordinatePercentage = yCoordinate / -200
+    var yCoordinatePercentage = yCoordinate / 200
     const DURATION = 10000
-    const PATTERN = [1000, 2000, 3000]
+    const PATTERN_FAST = [100, 100, 100]
+    const PATTERN_MEDIUM = [100, 400, 400]
+    const PATTERN_SLOW = [100, 700, 700]
+    const PATTERN_VERY_SLOW = [100, 1000, 1000]
 
     console.log("Coordinates are X: " + event.nativeEvent.contentOffset.x + "Y: " + event.nativeEvent.contentOffset.y)
     console.log("Percentage is " + yCoordinatePercentage)
     if (yCoordinatePercentage <= .2 ) {
-      Vibration.vibrate(PATTERN)
+      Vibration.vibrate(PATTERN_FAST)
+
+    }
+    else if (yCoordinatePercentage <= .4 && yCoordinatePercentage > .2 ) {
+      Vibration.vibrate(PATTERN_MEDIUM)
+
+    }
+    else if (yCoordinatePercentage <= .6 && yCoordinatePercentage > .8 ) {
+      Vibration.vibrate(PATTERN_SLOW)
+
+    }
+    else if (yCoordinatePercentage <= .8 && yCoordinatePercentage > 1 ) {
+      Vibration.vibrate(PATTERN_VERY_SLOW)
 
     }
   }
