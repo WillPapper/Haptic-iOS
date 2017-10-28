037427046eaf229e785a6c5cc09fd51c836ab7c9
Will Papper <contact@papper.me>
2017-10-28 14:10:53 -0700

    Triggered vibration based on scroll placement

--- Haptic/App.js
+++ Haptic/App.js
@@ -6,15 +6,21 @@ const width = '100%';
 export default class App extends React.Component {
 
   handleScroll(event) {
-    console.log("Coordinates are X: " + event.nativeEvent.contentOffset.x + "Y: " + event.nativeEvent.contentOffset.y)
     // Assume that position always starts at the top
-    
+    // Starting point for Y is 0, ending point for Y is -200
+    var yCoordinate = event.nativeEvent.contentOffset.y
+    var yCoordinatePercentage = yCoordinate / -200
+    const DURATION = 10000
+    const PATTERN = [1000, 2000, 3000]
+
+    console.log("Coordinates are X: " + event.nativeEvent.contentOffset.x + "Y: " + event.nativeEvent.contentOffset.y)
+    console.log("Percentage is " + yCoordinatePercentage)
+    if (yCoordinatePercentage <= .2 ) {
+      Vibration.vibrate(PATTERN)
+    }
   }
 
   render() {
-      const DURATION = 10000
-      const PATTERN = [1000, 2000, 3000]
-      Vibration.vibrate(PATTERN)
     return (
       <ScrollView style={styles.container} onScroll = { this.handleScroll } scrollEventThrottle = {16}>
       <View style={styles.text}>
