31b2796382113ebd8ed72b2ed4a93427e30d64a5
Will Papper <contact@papper.me>
2017-10-28 13:25:36 -0700

    Set up onClick for TouchableHighlight
    Need to fix view, but it's closer to working

--- Haptic/App.js
+++ Haptic/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View, Vibration } from 'react-native';
+import { StyleSheet, Text, View, Vibration, TouchableHighlight, Alert } from 'react-native';
 
 export default class App extends React.Component {
+  onClick() {
+    Alert.alert("Button clicked!")
+  }
+
   render() {
       const DURATION = 10000
       const PATTERN = [1000, 2000, 3000]
       Vibration.vibrate(PATTERN)
     return (
+      <TouchableHighlight onPress = { (event) => this.onClick(event) }>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
+      </TouchableHighlight>
     );
   }
 }
